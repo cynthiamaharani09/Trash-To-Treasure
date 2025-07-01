8567357e384f0378ff28e48b2aa9fef86ffaf910
Cynthia Maharani <cynthiamaharanii12@gmail.com>
2025-07-02 00:55:24 +0700

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "TRASH2TREASURE_UAS"
+include(":app")
+ 
